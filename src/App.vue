bfa187bb38ba2d5023626354aba88c172be37617
NERFOO <james.jaime.1105@gmail.com>
2022-11-24 21:45:36 +0100

    GenerarQR

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GenerarQr></GenerarQr>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GenerarQr from './components/GenerarQr.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GenerarQr
   }
 }
 </script>
---
+++ src/components/GenerarQr.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label>Introduzca la url</label><br/>
+        <input type="text" v-model="urlPagina"><br/>
+
+        <label>En caso de querer un logo en el centro del QR</label><br/>
+        <input type="text" v-model="urlLogo"><br/>
+
+        <hr/>
+        <vue-qr
+            :logoSrc="this.urlLogo"
+            :text="this.urlPagina"
+            :size="200"
+            dotScale="0.1"
+        ></vue-qr>
+        <hr/>
+    </div>
+</template>
+
+<script>
+import vueQr from 'vue-qr/src/packages/vue-qr.vue'
+
+export default {
+    name : "GenerarQr" ,
+    data() {
+        return {
+            urlLogo : "" ,
+            urlPagina : ""
+        }
+    } ,
+    components : {
+        vueQr
+    }
+}
+</script>
+
+<style>
+
+</style>
